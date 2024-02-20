061c311be45bb71565deeb0ecf72567e53319469
Hinckley28 <144728931+Hinckley28@users.noreply.github.com>
2024-02-20 11:27:35 -0700

    Add files via upload

---
+++ reverseInsert.js
@@ -0,0 +1,12 @@
+function insertionSortReverse(arr) {
+    for(let i = arr.length - 1; i >= 0; i--) {
+        let val = arr[i];
+        let j;
+        for(j = i; j < arr.length && arr[j+1] < val; j++) {
+            arr[j] = arr[j+1];
+        }
+    arr[j] = val;
+    }
+
+    return arr;
+}
